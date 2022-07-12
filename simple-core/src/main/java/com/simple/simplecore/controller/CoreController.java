e50ae59fcb68450835291e701e0eec1a45b900ee
zhoachegn <zhoachegn@163.com>
2022-07-12 17:34:42 +0800

    添加 注册中心

--- simple-core/src/main/java/com/simple/simplecore/controller/CoreController.java
+++ simple-core/src/main/java/com/simple/simplecore/controller/CoreController.java
@@ -1,6 +1,9 @@
 package com.simple.simplecore.controller;
 
 import com.simple.simplecore.domain.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 /*
@@ -11,9 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/simple/core/test")
 public class CoreController {
+//    private static final Logger LOGGER = LogManager.getLogger(CoreController.class);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CoreController.class);
 
     @RequestMapping(value = "/sayHello", method = RequestMethod.GET)
     public Response sayHello(@RequestParam String name) {
+        LOGGER.info("one request of: {}", name);
         return Response.successful("Hello " + name);
     }
 }
